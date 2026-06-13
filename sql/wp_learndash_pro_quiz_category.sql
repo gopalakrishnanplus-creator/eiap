c818a17b81416d1a3c1c43d641631666e7702321
SanthoshLSA <santhosh.ananth6@gmail.com>
2026-06-13 19:00:36 +0530

    Add .gitignore

--- sql/wp_learndash_pro_quiz_category.sql
+++
@@ -1,9 +0,0 @@
-/*M!999999\- enable the sandbox mode */ 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `wp_learndash_pro_quiz_category` (
-  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(200) NOT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
